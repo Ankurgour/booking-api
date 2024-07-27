e2aa62ca3a0a2e73d40c06954fd1ad63a3c2a04a
Ankur Gour <ankurgour2003@gmail.com>
2024-07-27 16:54:47 +0530

    solved cors error

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ configDotenv();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://booking-api-ffn0.onrender.com"],
+}));
 app.use(cookieParser());
 app.use(bodyParser.json());
 const options = {
